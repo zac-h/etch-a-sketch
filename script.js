d4f02bbf57c807273e05917350c7324ed89ce97f
zac-h <89298052+zac-h@users.noreply.github.com>
2022-03-01 20:34:37 +1030

    Added reset button to clear grid and ask user for size of new grid.

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
 const container = document.querySelector('#container');
 
-const defaultSize=16;
+const defaultSize=8;
 
 function setupGrid(size){
     for(let i=1; i<=size; i++){
         const divColumn = document.createElement('div');
         divColumn.classList.add('column');
-        divColumn.classList.add(i);
         container.appendChild(divColumn);
 
         for(let j=1; j<=size; j++){
-            const divRow = document.createElement('div');
-            divRow.classList.add('row', `column:${i}`, `row:${j}`);
-            divColumn.appendChild(divRow);
-            divRow.addEventListener('mouseenter',function(e){
+            const divSquare = document.createElement('div');
+            divSquare.classList.add('square');
+            divColumn.appendChild(divSquare);
+            divSquare.addEventListener('mouseenter',function(e){
                 e.target.classList.add('hover');
-                //e.target.style.background = 'blue';
             });
-
-
-            /*divRow.addEventListener('mouseleave',function(e){
-                e.target.classList.remove('hover');
-                //e.target.style.background = '';
-            });*/
-
-
         }
     }
+}
 
-    /*const rows = document.querySelectorAll('div.row');
-
-    rows.forEach( (row) => {
-        row.addEventListener('hover',()=>{
-            alert(row.className);
-        });
-    });
-    console.log(rows);*/
+function clearGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
 
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () =>{
-
+    clearGrid();
+    newSize = prompt('Input grid size: ');
+    if(newSize>100){
+        newSize = prompt('Please enter value less than 100:');
+    }
+    setupGrid(newSize);
 });
 
 window.onload = () => {
